4a220cbcba1f1ec68510d98093c26580c1b7062b
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-07 22:46:10 -0300

    feat: Implementação do formulário de login do Uiverse, criação de arquivos SCSS e personalização dos estilos
    - Adiciona o modelo de formulário de login do Uiverse ao projeto.
    - Cria arquivos SCSS para melhor organização e manutenção dos estilos.
    - Ajusta os estilos do formulário de login para uma aparência mais personalizada.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Login from './assets/pages/Login/Login';
+import Dashboard from './assets/pages/Dashboard';
+import Projects from './assets/pages/Projects';
+import Reports from './assets/pages/Reports';
+import Tasks from './assets/pages/Tasks';
+import NotFound from './assets/pages/NotFound';
 
 function App() {
 
