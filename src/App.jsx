1a3181e6a31091973bd71930f5edfbc876b9667c
Uriasmanu <maanoelaurias@gmail.com>
2024-08-07 21:18:02 -0300

    - feat: configuração das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Reports" element={<Reports/>} />
+        <Route path="/Tasks" element={<Tasks />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
